f48a36eab6d33c74f67bdd36c9b13d4c9de20e30
Ben Wright <benjamin.wright@nielsen.com>
2020-01-23 08:38:08 +0000

    adding redis as a caching layer for the processed data

--- metrics-api/src/server.js
+++ metrics-api/src/server.js
@@ -1,16 +1,28 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const KafkaListener = require('./kafka-listener');
+const RedisWriter = require('./redis-writer');
 
 const app = new Koa();
 const router = new Router();
 
-const host = process.env['KAFKA_HOST'] || 'kafka';
-const port = process.env['KAFKA_PORT'] || 9092
+const kafkaHost = process.env['KAFKA_HOST'] || 'kafka';
+const kafkaPort = process.env['KAFKA_PORT'] || 9092;
+const redisHost = process.env['REDIS_HOST'] || 'redis';
+const redisPort = process.env['REDIS_PORT'] || 6379;
 
-const listener = new KafkaListener(host, port);
-listener.addTopic('colors.processed', (key, value) => console.log(key, value));
-listener.addTopic('colors.list', (key, value) => console.log(key, value));
+const listener = new KafkaListener(kafkaHost, kafkaPort);
+const writer = new RedisWriter(redisHost, redisPort);
+
+listener.addTopic('colors.processed', (key, value) => {
+  console.log(key, value);
+  writer.set(key, value);
+});
+listener.addTopic('colors.list', (key, value) => {
+  console.log(key, value);
+  writer.set(key, value);
+});
+
 listener.start();
 
 router.get('/', (ctx, next) => {
